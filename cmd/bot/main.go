f9aef97df20e01463853e97c98a0ae8b26def593
LashinCHE <egor.cherevichin1337@gmail.com>
2024-06-16 21:16:52 +0300

    add some logic and try to isolate it

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
 
+	"github.com/LashinCHE/golang_test_bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
@@ -22,6 +24,8 @@ func main() {
 		Timeout: 30,
 	}
 
+	productService := product.NewService()
+
 	updates := bot.GetUpdatesChan(updateConfig)
 	for update := range updates {
 		if update.Message == nil {
@@ -31,6 +35,8 @@ func main() {
 		switch update.Message.Command() {
 		case "help":
 			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message, productService)
 		default:
 			DefaultBehaviour(bot, update.Message)
 		}
@@ -38,13 +44,28 @@ func main() {
 }
 
 func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help")
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n"+
+			"/list - list {entities}",
+	)
 
 	bot.Send(msg)
 }
 
 func DefaultBehaviour(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Your text is: "+inputMessage.Text)
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Your input message: "+inputMessage.Text)
+
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	products := productService.List()
+	textAns := "Items:\n\n"
+	for _, product := range products {
+		textAns += product.Title + "\n"
+	}
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, textAns)
 
 	bot.Send(msg)
 }
---
+++ internal/service/product/models.go
@@ -0,0 +1,12 @@
+package product
+
+var allProducts = []Product{
+	{Title: "One"},
+	{Title: "Two"},
+	{Title: "Three"},
+	{Title: "Four"},
+}
+
+type Product struct {
+	Title string
+}
